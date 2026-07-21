0bcda68eb6a956aa71bdda0b9be54cc5e2259d90
kegpoison <ugglefluffie@gmail.com>
2026-07-22 08:31:19 +1000

    feat(fish): Add cargo installations to $PATH
    Reorganises PATH management into a more aesthetically pleasing setup,
    adds ~/.cargo/bin to PATH.

--- packages/fish/.config/fish/config.fish
+++ packages/fish/.config/fish/config.fish
@@ -1,12 +1,14 @@
 if status is-interactive
-    fish_add_path ~/bin
     set EDITOR vim
 
     if not set -q SSH_AUTH_SOCK
         eval (ssh-agent -c) > /dev/null
     end
 
+    # PATH modifications
     if test (uname) = Darwin
         fish_add_path "/opt/homebrew/bin"
     end
+    fish_add_path "~/.cargo/bin"
+    fish_add_path "~/bin"
 end
